f572c6c533a64c2ce8f6266b3f688acf6df1e540
Irene Rivero <irene.rivero.casal@gmail.com>
2024-03-20 20:09:34 +0100

    pwd works with more than one arg

--- src/builtins/pwd.c
+++ src/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: irivero- <irivero-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 15:51:39 by irivero-          #+#    #+#             */
-/*   Updated: 2024/03/18 13:38:18 by irivero-         ###   ########.fr       */
+/*   Updated: 2024/03/20 18:26:54 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +31,13 @@ char	*our_pwd(char **av, int pflag)
 	path = malloc(1024 * sizeof(char));
 	while (av[i] != NULL && av[i])
 		i++;
-	if (i > 1)
-	{
-		printf("pwd: too many arguments!\n");
-		return (NULL);
-	}
 	if (getcwd(path1, sizeof(path1)) == NULL)
 	{
 		perror("getcwd");
 		g_exit_status = 127;
 		return (NULL);
 	}
-	if (ft_strcmp(av[0], "pwd") == 0)
+	if (ft_strcmp(av[0], "pwd") == 0 || i > 1)
 		printf("%s\n", path1);
 	g_exit_status = 0;
 	ft_strlcpy(path, path1, b_strlen(path1) + 1);
