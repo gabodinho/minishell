7df5a83d992c6f0abeb1faebb0af77f5c036f83d
Irene Rivero <irene.rivero.casal@gmail.com>
2024-04-02 14:33:59 +0200

    norminette append

--- src/tokenizer/token_append.c
+++ src/tokenizer/token_append.c
@@ -6,57 +6,31 @@
 /*   By: irivero- <irivero-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 16:52:06 by irivero-          #+#    #+#             */
-/*   Updated: 2024/04/02 13:58:59 by irivero-         ###   ########.fr       */
+/*   Updated: 2024/04/02 14:33:03 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tokenizer.h"
 
-void	create_and_add_token(int type, char *str, t_token **token_lst)
+void	process_current_char(char **line, t_token *lst)
 {
-	t_token	*token;
-
-	token = create_token(str, type);
-	if (token)
-		token_lst_add_back(token_lst, token);
-}
-
-void	process_current_char(int type, char **line, t_token **lst)
-{
-	char	*str;
-	
-	str = NULL;
-	if (**line == '>' || **line == '<')
+	if (*(*line + 1) == **line)
 	{
-		if (*(*line + 1) == **line)
-		{
-			if (**line == '>')
-				str = ft_strdup(">>");
-			else
-				str = ft_strdup("<<");
-			(*line)++;
-		}
+		if (**line == '>')
+			lst->str = ft_strdup(">>");
 		else
-		{
-			if (**line == '>')
-				str = ft_strdup(">");
-			else
-				str = ft_strdup("<");
-		}
+			lst->str = ft_strdup("<<");
+		(*line)++;
 	}
-	//else if (**line == '|')
-	//	str = ft_strdup("|");
-	create_and_add_token(type, str, lst);
+	else
+	{
+		if (**line == '>')
+			lst->str = ft_strdup(">");
+		else
+			lst->str = ft_strdup("<");
+	}
 }
 
-int	add_separator_token(int type, char **line, t_token **token_lst)
-{
-	process_current_char(type, line, token_lst);
-	(*line)++;
-	return (1);
-}
-
-/*
 int	add_separator_token(int type, char **line, t_token **token_lst)
 {
 	t_token	*token;
@@ -65,29 +39,13 @@ int	add_separator_token(int type, char **line, t_token **token_lst)
 	if (!token)
 		return (0);
 	if (**line == '>' || **line == '<')
-	{
-		if (*(*line + 1) == **line)
-		{
-			if (**line == '>')
-				token->str = ft_strdup(">>");
-			else
-				token->str = ft_strdup("<<");
-			(*line)++;
-		}
-		else
-		{
-			if (**line == '>')
-				token->str = ft_strdup(">");
-			else
-				token->str = ft_strdup("<");
-		}
-	}
+		process_current_char(line, token);
 	else if (**line == '|')
 		token->str = ft_strdup("|");
 	token_lst_add_back(token_lst, token);
 	(*line)++;
 	return (1);
-}*/
+}
 
 int	process_command(char **line, t_token **token_lst)
 {
