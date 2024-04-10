3d4c5e32a6a3c7dd70ab752399e226529ee9048c
Irene Rivero <irene.rivero.casal@gmail.com>
2024-04-10 16:36:15 +0200

    expander finished

--- src/expander/pre_expand.c
+++ src/expander/pre_expand.c
@@ -6,13 +6,62 @@
 /*   By: irivero- <irivero-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 09:39:49 by irivero-          #+#    #+#             */
-/*   Updated: 2024/04/10 14:11:53 by irivero-         ###   ########.fr       */
+/*   Updated: 2024/04/10 16:35:55 by irivero-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tokenizer.h"
 #include "expander.h"
 
+char	*expand_str(t_exp *exp, t_string_info *info)
+{
+	int		start_d;
+	int		end_d;
+	char	*new_str;
+	char	*str;
+
+	if (exp->quotes == '\'')
+		return (ft_strdup(exp->str));
+	str = ft_strdup(exp->str);
+	start_d = is_token_in_env(str);
+	while (start_d != -1)
+	{
+		end_d = find_dollar(str, start_d + 1);
+		new_str = create_string(info, str, start_d, end_d);
+		free(str);
+		str = new_str;
+		start_d = is_token_in_env(str);
+	}
+	return (str);
+}
+
+void	expander(t_list *env, t_token *token, int exit_status)
+{
+	t_exp			*exp_tok;
+	t_exp			*ptr;
+	char			*res;
+	t_string_info	info;
+
+	info.env = env;
+	info.exit_status = exit_status;
+	while (token)
+	{
+		res = NULL;
+		exp_tok = split_quotes(token->str);
+		ptr = exp_tok;
+		while (ptr)
+		{
+			res = join_and_free(res, expand_str(ptr, &info));
+			ptr = ptr->next;
+		}
+		free(token->str);
+		token->str = res;
+		clear_exp(&exp_tok);
+		token = token->next;
+	}
+}
+
+/*
 static char	*expand_str(t_exp *exp, t_list *env, int exit_status)
 {
 	int			start_d;
@@ -33,8 +82,9 @@ static char	*expand_str(t_exp *exp, t_list *env, int exit_status)
 		start_d = is_token_in_env(str);
 	}
 	return (str);
-}
+}*/
 
+/*
 void	expander(t_list *env, t_token *token, int exit_status)
 {
 	t_exp		*exp_tok;
@@ -50,10 +100,4 @@ void	expander(t_list *env, t_token *token, int exit_status)
 		{
 			res = join_and_free(res, expand_str(ptr, env, exit_status));
 			ptr = ptr -> next;
-		}
-		free(token -> str);
-		token -> str = res;
-		clear_exp(&exp_tok);
-		token = token -> next;
-	}
-}
+	*/
