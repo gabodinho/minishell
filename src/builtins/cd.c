37ce4c03d043f19c1201e18e49100d57061428aa
Gabriel Giertzuch <ggiertzu@c4b9c5.42berlin.de>
2024-04-12 21:43:33 +0200

    added error message for missing home dir

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -67,6 +67,7 @@ int	our_cd_internal(char **av, t_list *env_list, char *old_pwd)
 	if (!new_dir)
 	{
 		free(old_pwd);
+		ft_putendl_fd("minishell: cd : HOME not set", 2);
 		return (1);
 	}
 	new_pwd = change_directory_and_get_new_pwd(new_dir);
